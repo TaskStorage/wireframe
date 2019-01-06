40d8593d9d378b789c14364b348b70c702b4e7cc
Konstantin <KonstTask@gmail.com>
2019-01-06 18:52:09 +0200

    MySQL Create Read Delete Search

--- src/main/java/com/taskstorage/wireframe/controller/MainController.java
+++ src/main/java/com/taskstorage/wireframe/controller/MainController.java
@@ -1,16 +1,61 @@
 package com.taskstorage.wireframe.controller;
 
+import com.taskstorage.wireframe.domain.Task;
+import com.taskstorage.wireframe.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+    @Autowired
+    private TaskRepository taskRepository;
+
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "welcome";
     }
 
+    @GetMapping("/tasks")
+    public String list(Model model) {
+        Iterable<Task> tasks = taskRepository.findAll();
+        model.addAttribute("tasks", tasks);
+        return "main";
+    }
+
+    @PostMapping("/addTask")
+    public String add(@RequestParam String description, @RequestParam String content,Model model) {
+
+        Task task = new Task(description, content);
+        taskRepository.save(task);
+
+        return "redirect:/tasks";
+    }
+    @PostMapping("/delTask/{id}")
+    public String delete(@PathVariable Long id, Model model) {
+
+        taskRepository.deleteById(id);
+
+        return "redirect:/tasks";
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam String searchTag, Model model) {
+
+        Iterable<Task> tasks;
+
+        if (searchTag != null && !searchTag.isEmpty()) {
+            tasks = taskRepository.findByDescriptionContainingOrContentContaining(searchTag, searchTag);
+        } else {
+            tasks = taskRepository.findAll();
+        }
+
+        model.addAttribute("tasks", tasks);
+        return "main";
+    }
 }
