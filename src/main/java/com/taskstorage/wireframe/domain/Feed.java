239433fb0beddd8eee8f724300157880e902f36c
Konstantin <KonstTask@gmail.com>
2019-02-11 22:51:06 +0200

    rss template

--- src/main/java/com/taskstorage/wireframe/domain/Feed.java
+++ src/main/java/com/taskstorage/wireframe/domain/Feed.java
@@ -1,12 +1,10 @@
 package com.taskstorage.wireframe.domain;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
-@Entity
 public class Feed {
 
     @Id
